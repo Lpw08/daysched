ea78ffefde6701991e12a083d68c6ce7efd8e123
luke weber <lukedriftingweber@gmail.com>
2022-01-17 21:37:28 +1030

    code check-submitting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  document.getElementById("currentDate").value = date;
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  document.getElementById("currentTime").value = time;  
 function updateTime() {
     var x;
     var prefix = "time"
